0848991d4b013e1c6f4a7136666b164e364d2966
seshu babu karanam <108258542+seshubabukaranam@users.noreply.github.com>
2022-10-24 20:45:24 +0530

    feat- web page created

---
+++ index.js
@@ -0,0 +1,5 @@
+var billAmount = document.querySelector("#bill-amount");
+var cashAmountGiven = document.querySelector("#cash-amount");
+var btnCheak = document.querySelector("#btn-cheak");
+console.log("hi "+billAmount.value +" Hello");
+
